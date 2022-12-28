a3508d932cfb362101e91ec152f66318390be6e5
shs <godppun@naver.com>
2022-12-28 21:02:09 +0900

    react library 기본 설정

---
+++ React/Library/src/App.js
@@ -0,0 +1,12 @@
+import "./app.css";
+import Debounce from "./components/debounce/debounce";
+import LodashDebounce from "./components/lodashDebounce/debounce";
+import LodashThrottle from "./components/lodashThrottle/throttle";
+import Normal from "./components/normal/normal";
+import Throttle from "./components/throttle/throttle";
+
+function App() {
+  return <div className="app"></div>;
+}
+
+export default App;
